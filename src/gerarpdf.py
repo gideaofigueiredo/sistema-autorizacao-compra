a2b9219bb9e34826ec6737d3e8a3b8e0c26e3875
Gideao <gideaosantosf@gmail.com>
2026-03-21 16:11:24 -0300

    initial commit

---
+++ src/gerarpdf.py
@@ -0,0 +1,91 @@
+from fpdf import FPDF, XPos, YPos
+from fpdf.enums import CellBordersLayout
+from fpdf.fonts import FontFace
+from datetime import datetime
+
+def gerar_pdf(dados: dict):
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_auto_page_break(auto=True, margin=10) 
+
+    pdf.image("src/assets/logo.svg", x=11, y=12, w=33)
+
+    pdf.set_font("helvetica", size=14)
+    with pdf.table(gutter_height=3, gutter_width=3) as table:
+        row = table.row()
+        row.cell("")
+        row.cell("Sistema de Autorização", colspan=3,align="C")
+        row.cell(f"Nº {dados['numero']}", align="C")
+
+    pdf.ln(3)
+    pdf.set_font("helvetica", size=10)
+    with pdf.table(headings_style=FontFace(emphasis=None)) as table:
+        row = table.row()
+        row.cell(f" Emitido por: Fulano de Tal")
+        row.cell(f" Data de emissão: {datetime.now().strftime('%d/%m/%Y')} às {datetime.now().strftime('%H:%M:%S')}")
+
+    pdf.set_font("helvetica", size=14, style="B")
+    pdf.ln(10)
+    pdf.cell(190, 10,text="Autorização de Compra", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='C')
+    pdf.set_font("helvetica", size=12)
+    pdf.cell(190, 10, text="Documento oficial para liberação de serviços", new_x=XPos.LMARGIN, new_y=YPos.NEXT, align='C')
+
+    pdf.ln(10)
+    pdf.set_font("helvetica", size=12)
+    with pdf.table() as table:
+        row = table.row()
+        row.cell(" Fornecedor", border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT)
+        row.cell(" Nº do orçamento:", border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT)
+
+        row = table.row()
+        row.cell(f" {dados['fornecedor']}", border=CellBordersLayout.BOTTOM | CellBordersLayout.LEFT | CellBordersLayout.RIGHT)
+        row.cell(f" {dados['orcamento']}", border=CellBordersLayout.BOTTOM | CellBordersLayout.RIGHT)
+
+    pdf.ln(5)
+    with pdf.table() as table:
+        row = table.row()
+        row.cell(" Placa do veículo", border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT)
+        row.cell(" Quilometragem", border=CellBordersLayout.RIGHT | CellBordersLayout.TOP)
+
+        row = table.row()
+        row.cell(f" {dados['placa']}", border=CellBordersLayout.BOTTOM | CellBordersLayout.LEFT | CellBordersLayout.RIGHT)
+        row.cell(f" {dados['km']}", border=CellBordersLayout.BOTTOM | CellBordersLayout.RIGHT)
+
+    pdf.ln(10)
+    with pdf.table() as table:
+        row = table.row()
+        row.cell("Detalhamento de valores", colspan=2, border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT, align="C")
+
+        row = table.row()
+        row.cell("Valor das Peças", align="C")
+        row.cell("Valor da Mão de Obra", align="C")
+
+        row = table.row()
+        row.cell(f"R$ {dados['valor_pecas']}", align="C")
+        row.cell(f"R$ {dados['valor_mao_de_obra']}", align="C")
+
+    pdf.ln(10)
+    with pdf.table() as table:
+        row = table.row()
+        row.cell("Valor total autorizado", colspan=2, border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT, align="C")
+
+        row = table.row()
+        row.cell(f"R$ {float(dados['valor_pecas']) + float(dados['valor_mao_de_obra'])}", colspan=2,align="C")
+
+    pdf.ln(10)
+    with pdf.table() as table:
+        row = table.row()
+        row.cell("Observações", colspan=2, border=CellBordersLayout.TOP | CellBordersLayout.LEFT | CellBordersLayout.RIGHT, align="C")
+
+        row = table.row()
+        row.cell(f"{dados['observacao']}", colspan=2, align="L")
+    
+    pdf.ln(5)
+    pdf.set_font("helvetica", size=10)
+    pdf.write(text="Solicitamos que o número da Ordem de Compra (OC) seja informado na nota fiscal.")
+
+    pdf.set_y(-25)
+    pdf.set_font("helvetica", size=8)
+    pdf.write(text="Informações do rodapé: CNPJ, endereço, telefone, etc.")
+
+    pdf.output(f"storage/Autorizacão de Compra {dados['numero']}.pdf")
---
+++ src/main.py
@@ -0,0 +1,70 @@
+import flet as ft
+import gspread
+from datetime import datetime
+from gerarpdf import gerar_pdf
+import os
+
+# AUTENTICACAO
+nome_app = "AutCompraSystem"
+caminho_creds = os.path.join(os.path.join(os.getenv('APPDATA'), nome_app), "creds.json")
+
+# 3. Verifica se a pasta existe, se não, cria (útil na primeira execução)
+if not os.path.exists(os.path.join(os.getenv('APPDATA'), nome_app)):
+    os.makedirs(os.path.join(os.getenv('APPDATA'), nome_app))
+
+print(f"Buscando credenciais em: {caminho_creds}")
+gc = gspread.service_account(filename=caminho_creds)
+
+wks = gc.open("AutComprasMaster").sheet1
+
+def main(page: ft.Page):
+    page.title = "AutCompraSystem"
+    page.add(ft.Text(value="SISTEMA DE AUTORIZACAO DE COMPRAS", align=ft.Alignment.CENTER))
+    
+    #CAMPOS DE TEXTO
+    data = datetime.now().strftime("%d/%m/%Y")
+    fornecedor = ft.TextField(label = "Fornecedor: ", align=ft.Alignment.CENTER)
+    orcamento = ft.TextField(label = "Número do orçamento: ", align=ft.Alignment.CENTER)
+    placa = ft.TextField(label = "Placa: ", align=ft.Alignment.CENTER)
+    km = ft.TextField(label = "KM: ", align=ft.Alignment.CENTER)
+    valor_pecas = ft.TextField(label = "Valor das peças: ", input_filter=ft.InputFilter(allow=True, regex_string=r"[0-9]", replacement_string=""),align=ft.Alignment.CENTER)
+    valor_mao_de_obra = ft.TextField(label = "Valor da mão de obra: ", align=ft.Alignment.CENTER)
+    observacao = ft.TextField(label = "Observações: ", align=ft.Alignment.CENTER)
+
+    #BOTAO DE ENVIO
+    def enviar(e):
+        ultimo_numero = str(int(wks.cell(2, 1).value[-3:])+1).zfill(3)
+        numero = f"{datetime.now().strftime('%m%y')}"+f"-{ultimo_numero}"
+        page.add(ft.Text(f"Registro enviado: {numero}", align=ft.Alignment.CENTER))
+        wks.insert_row([numero, data, fornecedor.value, orcamento.value, placa.value, km.value, valor_pecas.value, valor_mao_de_obra.value, observacao.value], index=2)
+        
+        dados = {
+        "numero": numero,
+        "data": data,
+        "fornecedor": fornecedor.value,
+        "orcamento": orcamento.value,
+        "placa": placa.value,
+        "km": km.value,
+        "valor_pecas": valor_pecas.value,
+        "valor_mao_de_obra": valor_mao_de_obra.value,
+        "observacao": observacao.value
+        }
+
+        #GERAR PDF
+        gerar_pdf(dados)
+
+        #LIMPA OS CAMPOS DE TEXTO APÓS O ENVIO
+        fornecedor.value = ""
+        orcamento.value = ""
+        placa.value = ""
+        km.value = ""
+        valor_pecas.value = ""
+        valor_mao_de_obra.value = ""
+        observacao.value = ""
+
+    benviar = ft.Button("Enviar", on_click=enviar, align=ft.Alignment.CENTER)
+    
+    page.add(fornecedor, orcamento, placa, km, valor_pecas, valor_mao_de_obra, observacao)
+    page.add(benviar)
+
+ft.run(main)
