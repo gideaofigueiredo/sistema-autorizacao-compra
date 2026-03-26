2de958fe33a6a68278ebdaad140c29d66220e293
Gideao <gideao.figueiredo@hotmail.com>
2026-03-26 12:04:40 -0300

    feat: torna página cadastro funcional

--- src/views/cadastros.py
+++ src/views/cadastros.py
@@ -1,6 +1,21 @@
 import flet as ft
+import json
 
 def cadastros(page: ft.Page):
+    empresa = ft.TextField(label="Empresa:")
+    comprador = ft.TextField(label="Comprador:")
+    rodape = ft.TextField(label="Rodapé:")
+
+    def salvar_dados(e):
+        with open("storage/dados_usuario.json", "w", encoding='utf-8') as f:
+            json.dump({
+                "empresa": empresa.value,
+                "comprador": comprador.value,
+                "rodape": rodape.value
+            }, f, ensure_ascii=False, indent=4)
+        ft.SnackBar("Dados salvos com sucesso!").open=True
+        page.update()
+
     return ft.View(
         route="/cadastros",
         controls=[
@@ -9,6 +24,9 @@ def cadastros(page: ft.Page):
                 bgcolor=ft.Colors.SURFACE_BRIGHT
         ),
         ft.Text("Página de Cadastros"),
+        empresa,
+        comprador,
+        rodape,
+        ft.ElevatedButton("Salvar", on_click=salvar_dados)
     ]
-
 )
